e45ac673ed82b1e8102a0989fd465937ebb6e177
volodya199514 <volodya199514@gmail.com>
2017-04-12 14:50:24 +0300

    end pogect

---
+++ components/MyWidget.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: devds
+ * Date: 12.04.17
+ * Time: 12:19
+ */
+class MyWidget
+{
+
+}
---
+++ components/views/my.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: devds
+ * Date: 12.04.17
+ * Time: 12:35
+ */
